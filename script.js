90c41d0f028bfb7b8639b4399cd8a1bf4fb18da3
Mikhail <mitenber@gmail.com>
2025-05-02 13:16:49 -0400

    javascript script able to take books and add them to array

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(name, author, length, status) {
+    if (!new.target) {
+        throw Error("You must use 'new' operator to call the book")
+    }
+
+    this.name = name;
+    this.author = author;
+    this.length = length;
+    this.status = status;
+
+    this.info = function() {
+        return name + " by " + author + ", " + length + " pages, " + status;
+    }
+
+    
 }
 
-function addBookToLibrary() {
+
+function addBookToLibrary(name, author, length, status) {
   // take params, create a book then store it in the array
+  const book = new Book(name, author, length, status);
+  myLibrary.push(book);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "Not Read");
+addBookToLibrary("Wind and Truth", "Brandon Sanderson", "1334","Read");
+
+for (i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i].info());
 }
