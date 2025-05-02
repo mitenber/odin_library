346931f73ebfe2edc043933419f0f74cab326085
Mikhail <mitenber@gmail.com>
2025-05-02 16:31:24 -0400

    adding function and form works

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-let screen = document.getElementById("shelf");
-
+const screen = document.getElementById("shelf");
+const addBookBtn = document.getElementById("add-book-btn");
+const bookForm = document.getElementById("book-form");
+const closeForm = document.getElementById("close-form")
+const submitForm = document.getElementById("submit-book")
 
 const myLibrary = [];
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, "Not Read");
+addBookToLibrary("Wind and Truth", "Brandon Sanderson", 1334,"Read");
+addBookToLibrary("Pyramids","Terry Pratchett", 368, "Reading" );
+addBookToLibrary("True Believer", "Jack Carr", 497, "Read")
+
+showBooks();
+
+const overlay = document.createElement("div");
+overlay.className = "form-overlay";
+document.body.appendChild(overlay);
+
+addBookBtn.addEventListener("click", function() {
+    bookForm.style.display = "block";
+    overlay.style.display = "block";
+})
+
+closeForm.addEventListener("click", function() {
+    bookForm.style.display = "none";
+    overlay.style.display = "none";
+})
+
+submitForm.addEventListener("click", function() {
+    let title = document.getElementById("book-title").value;
+    let author = document.getElementById("book-author").value;
+    let length = document.getElementById("book-length").value;
+    let status = document.getElementById("book-status").value;
+
+    addBookToLibrary(title,author,length,status);
+    bookForm.style.display = "none";
+    overlay.style.display = "none";
+    screen.innerHTML = "";
+    showBooks();
+})
 
 function Book(name, author, length, status) {
     if (!new.target) {
@@ -27,16 +64,22 @@ function addBookToLibrary(name, author, length, status) {
   myLibrary.push(book);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, "Not Read");
-addBookToLibrary("Wind and Truth", "Brandon Sanderson", 1334,"Read");
-addBookToLibrary("Pyramids","Terry Pratchett", 368, "Reading" );
-addBookToLibrary("True Believer", "Jack Carr", 497, "Read")
 
-for (i = 0; i < myLibrary.length; i++) {
-    console.log(myLibrary[i].info());
-    console.log(myLibrary[i].id);
-    let book = document.createElement("div");
-    book.className = "book-container";
-    book.innerHTML = myLibrary[i].info();
-    screen.appendChild(book);
+
+function showBooks() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i].info());
+        console.log(myLibrary[i].id);
+        let book = document.createElement("div");
+        book.className = "book-container";
+        book.innerHTML = myLibrary[i].info();
+        book.setAttribute('data-id', myLibrary[i].id);
+
+        let removeBtn = document.createElement("button");
+        removeBtn.className = 'remove-btn';
+        removeBtn.setAttribute('data-index', i);
+
+        book.appendChild(removeBtn);
+        screen.appendChild(book);
+    }
 }
