c79fadaff2e74e8fdd17915fac057288f8ba9c5d
Mikhail <mitenber@gmail.com>
2025-05-02 13:57:49 -0400

    front end view done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let screen = document.getElementById("shelf");
+
+
 const myLibrary = [];
 
 function Book(name, author, length, status) {
@@ -9,12 +12,12 @@ function Book(name, author, length, status) {
     this.author = author;
     this.length = length;
     this.status = status;
+    this.id = crypto.randomUUID();
 
     this.info = function() {
         return name + " by " + author + ", " + length + " pages, " + status;
     }
 
-    
 }
 
 
@@ -24,9 +27,16 @@ function addBookToLibrary(name, author, length, status) {
   myLibrary.push(book);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", "295", "Not Read");
-addBookToLibrary("Wind and Truth", "Brandon Sanderson", "1334","Read");
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 295, "Not Read");
+addBookToLibrary("Wind and Truth", "Brandon Sanderson", 1334,"Read");
+addBookToLibrary("Pyramids","Terry Pratchett", 368, "Reading" );
+addBookToLibrary("True Believer", "Jack Carr", 497, "Read")
 
 for (i = 0; i < myLibrary.length; i++) {
     console.log(myLibrary[i].info());
+    console.log(myLibrary[i].id);
+    let book = document.createElement("div");
+    book.className = "book-container";
+    book.innerHTML = myLibrary[i].info();
+    screen.appendChild(book);
 }
